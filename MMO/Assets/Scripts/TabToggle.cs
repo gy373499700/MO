e98695023d469decdabc82f33e044df7095569a2
郭袁 <373499700@qq.com>
2018-05-22 09:53:34 +0800

    update    aaaa

---
+++ MMO/Assets/Scripts/TabToggle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TabToggle : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject toggle1 = null;
+    public GameObject toggle2 = null;
+    public GameObject toggle3 = null;
+    IEnumerator Start()
+    {
+        yield return null;
+
+        toggle1.SetActive(true);
+        toggle2.SetActive(false);
+        toggle3.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
